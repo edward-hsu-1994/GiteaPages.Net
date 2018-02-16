cb5525ee0b4ea1097c692bb87b05f1d519209763
XuPeiYao <st89382000@gmail.com>
2018-02-16 23:37:33 +0800

    加入腳本注入Config模型

--- GiteaPages.Net/Models/GiteaConfig.cs
+++ GiteaPages.Net/Models/GiteaConfig.cs
@@ -7,5 +7,6 @@ namespace GiteaPages.Net.Models {
     public class GiteaConfig {
         public string Root { get; set; } = "";
         public string NotFound { get; set; } = "404.html";
+        public ScriptInjectionConfig[] ScriptInjection { get; set; }
     }
 }
---
+++ GiteaPages.Net/Models/ScriptInjectionConfig.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GiteaPages.Net.Models {
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ScriptInjectionPosition {
+        /// <summary>
+        /// 於Header起始後載入
+        /// </summary>
+        Header_Start,
+        /// <summary>
+        /// 於Header結束前載入
+        /// </summary>
+        Header_End,
+        /// <summary>
+        /// 於Body結束前載入
+        /// </summary>
+        Body_End
+    }
+
+    public class ScriptInjectionConfig {
+        public string Pattern { get; set; } = "*";
+        public ScriptInjectionPosition Position { get; set; }
+        public string Src { get; set; }
+    }
+}
